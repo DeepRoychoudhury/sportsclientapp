6045b628a7d88ed383cac822dedd227e803cc3af
DeepRoychoudhury <deeproydrc@gmail.com>
2020-07-23 00:09:18 +0100

    Changes for sports page configuring net/http for json parsing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_193614) do
+ActiveRecord::Schema.define(version: 2020_07_22_213721) do
+
+  create_table "sports", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sportskits", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
